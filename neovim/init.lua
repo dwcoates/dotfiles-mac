1063e9a36580726cba329b878854e243fece01cc
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-10-16 10:43:47 -0400

    Turn on one-dark theme.

--- neovim/init.lua
+++ neovim/init.lua
@@ -155,7 +155,7 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 

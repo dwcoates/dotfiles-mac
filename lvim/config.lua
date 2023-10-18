7170ad4798116dcd05c838cf67a737ea6d26ffe7
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-10-18 13:36:56 -0400

    Add lunarvim config.

---
+++ lvim/config.lua
@@ -0,0 +1,21 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+
+
+lvim.colorscheme = 'gruvbox'
+
+lvim.plugins = {
+  {'tpope/vim-rsi'},
+
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+}

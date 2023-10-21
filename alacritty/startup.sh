feb42f998573d2e3ad0128d104ae92395a587908
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-10-20 23:53:42 -0400

    Update alacritty config.

--- alacritty/startup.sh
+++ alacritty/startup.sh
@@ -7,12 +7,17 @@ SESSION="workspace"
 tmux new-session -d -s $SESSION -c ~ -n scratch
 
 # Create new windows for each subsequent project
-tmux new-window -t $SESSION -c ${CHESSCOM_PATH}/explanation-engine -n cee-microservice
-tmux new-window -t $SESSION -c ${CHESSCOM_PATH}/ceac -n ceac
-tmux new-window -t $SESSION -c ${WORKSPACE_PATH}/lua/project-term -n project-term
-tmux new-window -t $SESSION -c ${CHESSCOM_PATH}/explanation-engine -n explanation-engine
-tmux new-window -t $SESSION -c ${HOME}/.config/lvim/ -n lunarvim
-tmux new-window -t $SESSION -c ${WORKSPACE_PATH}/dotfiles -n dotfiles
+tmux new-window -t $SESSION -c "lvim ${CHESSCOM_PATH}/explanation-engine" -n cee-microservice
+tmux new-window -t $SESSION -c "lvim ${CHESSCOM_PATH}/ceac" -n ceac
+tmux new-window -t $SESSION -c "lvim ${WORKSPACE_PATH}/lua/project-term" -n project-term
+tmux new-window -t $SESSION -c "lvim ${CHESSCOM_PATH}/explanation-engine" -n explanation-engine
+tmux new-window -t $SESSION -c "lvim ${HOME}/.config/lvim/" -n lunarvim
+tmux new-window -t $SESSION -c "lvim ${WORKSPACE_PATH}/dotfiles" -n dotfiles
+
+# tmux new-window -t $SESSION -c "cd ${WORKSPACE_PATH}/dotfiles && lvim" -n dotfiles
+# tmux new-window -t $SESSION -c "cd ${WORKSPACE_PATH}/dotfiles && lvim" -n dotfiles
 
 # Attach to the session
 tmux attach -t $SESSION
+
+echo ${CHESSCOM_PATH}

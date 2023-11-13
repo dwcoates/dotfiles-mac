e3c0a727ae6c42303d222e76209a715ec8cf9548
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-11-12 23:23:36 -0500

    Fixup Treesitter import.

--- lvim/config.lua
+++ lvim/config.lua
@@ -57,6 +57,75 @@ lvim.plugins = {
     },
     build = ':TSUpdate',
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V',  -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            set_jumps = false, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]]"] = "@function.outer",
+              -- ["]["] = "@function.outer",
+              ["]o"] = "@loop.outer",
+              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+            },
+            goto_next_end = {
+              ["]["] = "@function.outer",
+              -- ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[["] = "@function.outer",
+              -- ["[]"] = "@function.outer",
+            },
+            goto_previous_end = {
+              ["[]"] = "@function.outer",
+              -- ["[]"] = "@class.outer",
+            },
+            goto_next = {
+              ["]d"] = "@conditional.outer",
+            },
+            goto_previous = {
+              ["[d"] = "@conditional.outer",
+            },
+          },
+          lsp_interop = {
+            enable = true,
+            border = 'none',
+            peek_definition_code = {
+              ["<leader>pf"] = "@function.outer",
+              ["<leader>pF"] = "@class.outer",
+            },
+          },
+        },
+      }
+    end,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
 
   { 'glepnir/dashboard-nvim' },
 }
@@ -348,33 +417,3 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 -- ]], false)
 
 lvim.builtin.which_key.mappings["q"] = { ":qa<CR>" }
-
--- TreeSitter text objects
-local ts = lvim.builtin.treesitter
-ts.textobjects = {
-    select = {
-        enable = true,
-        lookahead = true,  -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-            -- Use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["as"] = "@class.outer",
-            ["is"] = "@class.inner",
-            ["ai"] = "@conditional.outer",
-            ["ii"] = "@conditional.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["acm"] = "@comment.outer",
-            ["ia"] = "@parameter.inner",
-            ["aa"] = "@parameter.inner",
-        },
-    },
-    swap = {
-        enable = false,
-        -- swap_next = textobj_swap_keymaps,
-    },
-}
-

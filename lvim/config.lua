765fc135cdf5ea2385ed625a1e1328afbb44c96c
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-11-13 15:02:44 -0500

    Add more textobject bindings.

--- lvim/config.lua
+++ lvim/config.lua
@@ -68,15 +68,18 @@ lvim.plugins = {
             -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ["af"] = "@function.outer",
               ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+              ["as"] = "@class.outer",
+              ["is"] = "@class.inner",
+              ["ai"] = "@conditional.outer",
+              ["ii"] = "@conditional.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["ab"] = "@block.outer",
+              ["ib"] = "@block.inner",
+              ["acm"] = "@comment.outer",
+              ["aa"] = "@parameter.inner",
             },
             selection_modes = {
               ['@parameter.outer'] = 'v', -- charwise

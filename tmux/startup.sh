741b53f7b7b8ea988dd2244dd1182f172925e9dd
Dodge W. Coates <dodge.w.coates@gmail.com>
2024-05-08 11:58:21 -0500

    Update startup.

--- tmux/startup.sh
+++ tmux/startup.sh
@@ -31,14 +31,13 @@ function add_window {
   add_window 'ceac' $ce 2 
   add_window 'dotfiles' $dt 3 #':e ./lvim/config.lua' 
   add_window 'explanation-engine' $ee 4 
-  add_window 'engine-services' $es 5 
+  add_window 'ceac-services' $cs 5 
   add_window 'clue' $cl 6 
   add_window 'cee-interface' $ei 7 
   add_window 'platform' $pl 8 
   add_window 'definitions' $df 9 
-  add_window 'game-state-cache' "${CHESSCOM_PATH}/gametree" 10 
-  add_window 'ceac-clusters-proxy' $cp 11
-  add_window 'cee-microservice' $cm 12
-  add_window 'puzzlegen-service' $ps 13
+  add_window 'ceac-clusters-proxy' $cp 10
+  add_window 'cee-microservice' $cm 11
+  add_window 'puzzlegen-service' $ps 12
 
   tmux attach -t $SESSION
